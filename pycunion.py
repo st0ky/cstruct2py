94bef39157e32a03a45d9dce18f4f3fb4a746904
Your Name <you@example.com>
2017-12-09 21:08:43 +0200

    Move pyunion to new form

--- pycunion.py
+++ pycunion.py
@@ -1,6 +1,6 @@
-# from pycstruct import BasePyStruct
+# from pycbase import *
 
-class BasePyUnion(BasePyStruct):
+class BasePyUnion(PyBase):
     def __init__(self, buf=None, index=0):
         super(BasePyUnion, self).__init__(buf, index)
         self._cache = bytearray("\x00" * len(self))
@@ -8,84 +8,98 @@ class BasePyUnion(BasePyStruct):
             self._cache[0:len(self)] = self._buf[self._index:self._index + len(self)]
         self._last = type("\x01", (object,), {})()
         self._last.name = None
+        self._last.value = None
 
     def _flush_to_cache(self):
         if self._last.name:
-            self._fields[self._last.name].pack(self._cache, 0, self._last.value)
+            self._last.value.pack_into(self._cache, 0)
             self._last.name = None
 
-    def __getattr__(self, name):
-        print "[%d] __getattr__ %s" % (id(self), name)
-        if name not in self._fields:
-            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__, name))
+    def _set_field(self, val, name, cls):
+        if not name in self._fields:
+            raise KeyError(name)
 
         if self._last.name == name:
-            return self._last.value
-
-        self._flush_to_cache()
-
-        self._last.name = name
-        self._last.value = self._fields[name](self._cache)
-
-        return self._last.value
-
-    def __setattr__(self, name, val):
-        print "[%d] __getattr__ %s" % (id(self), name)
-        if name in ["_cache", "_last", "_buf", "_index"]:
-            self.__dict__[name] = val
+            self._last.value.val = val
             return
+        
+        self._flush_to_cache()
+        self._last.name = name
+        self._last.value = cls(self._cache, 0)
+        self._last.value.val = val
 
-        if name not in self._fields:
-            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__, name))
+    def _get_field(self, name, cls):
+        if not name in self._fields:
+            raise KeyError(name)
 
         if self._last.name == name:
-            self._last.value = self._fields[name].copy(val)
-            return
+            return self._last.value.val
 
         self._flush_to_cache()
-
         self._last.name = name
-        self._last.value = self._fields[name].copy(val)
+        self._last.value = cls(self._cache, 0)
 
-        return self._last.value
+        return self._last.value.val
+
+    @property
+    def val(self):
+        return self
+
+    @val.setter
+    def val(self, val):
+        if type(val) is type(self):
+            val._flush_to_cache()
+            self._last.name = None
+            self._cache[0:] = val._cache
+            return
+
+        if (type(val) in [str, bytearray] and len(val) >= len(self)):
+            tmp = type(self)(val)
+            self.val = tmp
+            return
+
+        raise ValueError(val)
+
+    def flush(self):
+        self._flush_to_cache()
+        if self._buf:
+            self.pack_into(self._buf, self._index)
+
+    def pack_into(self, buf, index=0):
+        self._flush_to_cache()
+        buf[index:index + len(self)] = self._cache
+
+    def parse_all(self):
+        pass
+    
 
 class MetaPyUnion(type):
     def __init__(cls, name, bases, d):
         super(MetaPyUnion, cls).__init__(name, (BasePyUnion,) + bases, d)
 
-        cls.size = max(v.size for v in cls._fields.values())
-        cls.__members__ = cls._fields.keys()
-        cls.__methods__ = []
-
     def __new__(cls, name, bases, d):
-        print type(d["_fields"]), d["_fields"]
-        assert "_fields" in d and type(d["_fields"]) in [dict, list]
-        if type(d["_fields"]) is list:
-            d["_fields"] = dict(d["_fields"])
+        assert "_fields" in d
+        assert type(d["_fields"]) in [list, tuple, dict]
+        if type(d["_fields"]) is dict:
+            d["_fields"] = d["_fields"].items()
+
+        size = 0
+        for (name, field_cls) in d["_fields"]:
+            assert type(name) is str, name
+            assert issubclass(field_cls, PyBase), field_cls
+            d[name] = property(
+                partial(BasePyUnion._get_field, name=name, cls=field_cls),
+                partial(BasePyUnion._set_field, name=name, cls=field_cls)
+                )
+            size = max(size, len(field_cls))
+
+        d["size"] = size
+        d["_fields"] = [name for (name, field_cls) in d["_fields"]]
+
         return type.__new__(cls, name, (BasePyUnion,) + bases, d)
-
-    def pack(cls, buf, index, inst):
-        inst._flush_to_cache()
-        buf[index:index + cls.size] = inst._cache
-        return buf
-
-    def unpack(cls, buf, index):
-        return cls(buf, index)
 
     def __len__(cls):
         return cls.size
-
-    def copy(cls, val):
-        if type(val) is cls:
-            ret = cls(val._buf, val._index)
-            val._flush_to_cache()
-            ret._cache = val._cache[:]
-            return ret
-        
-        if type(val) in [str, bytearray] and len(val) >= cls.size:
-            return cls(val)
-        
-        raise ValueError
 
 class A(object):
     __metaclass__ = MetaPyStruct
